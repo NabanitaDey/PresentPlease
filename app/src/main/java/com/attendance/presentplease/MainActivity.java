b8d99419afe28e8cae0c3f1362a18e3e98e7f92c
Nabanita Dey <nabanitadey825@gmail.com>
2020-02-19 16:34:55 +0530

    sign up page created,button created

--- app/src/main/java/com/attendance/presentplease/MainActivity.java
+++ app/src/main/java/com/attendance/presentplease/MainActivity.java
@@ -2,12 +2,17 @@ package com.attendance.presentplease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+//        btn1=(Button) findViewById(R.id.)
+//
     }
 }
--- app/src/main/java/com/attendance/presentplease/Signup.java
+++ app/src/main/java/com/attendance/presentplease/Signup.java
@@ -2,6 +2,7 @@ package com.attendance.presentplease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+
     }
 }
